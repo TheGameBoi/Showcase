e7428dabb29a8993dc4ba4a6d47cdedde0dc889c
websites <websites.unpinned074@passinbox.com>
2024-06-18 20:44:28 -0500

    Add app titles in 2 columns

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas
 import streamlit as st
 
 st.set_page_config(layout="wide")
@@ -13,6 +14,15 @@ with col2:
     I am currently studying the Python language in order to design apps for my favorite games, and possibly your favorite games!
     This is an online log of my coding journey thus far and into the future!
     """
-
-
     st.write(content)
+
+    col3, col4 = st.columns(2)
+    df = pandas.read_csv("data.csv", sep=";")
+
+    with col3:
+        for index, row in df[10:].iterrows():
+            st.header(row["title"])
+
+    with col4:
+        for index, row in df[:10].iterrows():
+            st.header(row["title"])
