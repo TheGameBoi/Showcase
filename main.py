76bb54b0211a7f910407d5718bc240009c387653
websites <websites.unpinned074@passinbox.com>
2024-06-18 21:13:32 -0500

    Added links

--- main.py
+++ main.py
@@ -16,13 +16,23 @@ with col2:
     """
     st.write(content)
 
-    col3, col4 = st.columns(2)
-    df = pandas.read_csv("data.csv", sep=";")
+content2 = """Below you can find some of the apps I have created using Python."""
 
-    with col3:
-        for index, row in df[10:].iterrows():
-            st.header(row["title"])
+st.write(content2)
 
-    with col4:
-        for index, row in df[:10].iterrows():
-            st.header(row["title"])
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.header(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.header(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
