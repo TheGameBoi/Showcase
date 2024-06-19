adb78af09c8e0b0ce0882e659287f478d404e479
websites <websites.unpinned074@passinbox.com>
2024-06-18 20:20:40 -0500

    Add columns, image, and description

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("GameBoi")
+    content ="""My name is GameBoi and I am an avid gamer, I enjoy gaming when I get the chance to, and computers have been my passion for the past 12 years.
+    I am currently studying the Python language in order to design apps for my favorite games, and possibly your favorite games!
+    This is an online log of my coding journey thus far and into the future!
+    """
+
+
+    st.write(content)
