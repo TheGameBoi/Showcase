9145d5b045d1616cc02ae93f00dfbaebd71a7d6d
GameBoi <thegameboi@proton.me>
2025-07-08 17:44:21 -0500

    Created Columns, Image, and Description.

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+st.set_page_config(layout="wide")
+
+
+col1, col2 = st.columns(2)
+
+
+
+with col1:
+    st.image("images/photo.png", width=600)
+
+with col2:
+    st.title("TheGameBoi")
+    content = ("""Hello! My name is GameBoi, and I am a software programmer, gamer, and music junky. This is my personal 
+    showcase website I built from scratch!""")
+
+    st.info(content)
